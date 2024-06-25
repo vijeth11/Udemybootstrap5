607d696141e90a4825c32247804919e53010aae4
Vijeth Belle <vijeth11@yahoo.co.in>
2024-06-25 21:17:24 +0530

    finished second project

---
+++ corso-website-starter/js/script.js
@@ -0,0 +1,14 @@
+// Add custom JavaScript here
+function userScroll(){
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= 50) {
+            navbar.classList.add('bg-dark');
+        } else {
+            navbar.classList.remove('bg-dark');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
